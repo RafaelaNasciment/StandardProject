65991f36ea9126e824072e271e8c3430e370db97
Rafaela Carvalho <Rafaelacarvalho.14@hotmail.com>
2024-05-09 16:20:23 -0300

    mapeando repository

---
+++ api-project-repository/Interfaces/IProductServiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using api_project_domain.Entity;
+
+namespace api_project_repository.Interfaces
+{
+    public interface IProductServiceRepository
+    {
+        Task CadastrarAsync(Produto entity);
+
+        Task EditarAsync(Produto entity);
+
+        Task<IList<Produto>> ListarAsync(Produto entity);
+
+        Task DeleteAsync(Produto entity);
+    }
+}
---
+++ api-project-repository/Repository/ProductService/ProductServiceRepository.cs
@@ -0,0 +1,53 @@
+﻿using api_project_domain.Entity;
+using api_project_infrastructure.Configuration;
+using api_project_repository.Interfaces;
+
+namespace api_project_repository.Repository.ProductService
+{
+    public class ProductServiceRepository : RepositorioBase<Produto>, IProductServiceRepository
+    {
+        private readonly string _alias = nameof(Produto);
+        public ProductServiceRepository(
+            ConfiguracoesGlobais configuracoesGlobais) : base(configuracoesGlobais)
+        {
+        }
+
+        public async Task CadastrarAsync(Produto entity)
+        {
+            await AddEntityAsync(
+                entity: entity,
+                partitionKey: entity.IdRegistro);
+        }
+
+        public async Task DeleteAsync(Produto entity)
+        {
+            await UpdateEntityAsync(
+                entity: entity,
+                partitionKey: entity.IdRegistro);
+        }
+
+        public async Task EditarAsync(Produto entity)
+        {
+            await DeleteEntity(
+                idContainer: entity.IdContainer, 
+                partitionKey: entity.IdRegistro);
+        }
+
+        public async Task<IList<Produto>> ListarAsync(Produto entity)
+        {
+            string query =
+                @$"
+                    SELECT 
+                        *
+                    FROM 
+                        {_alias} {_alias}
+                ";
+
+            IList<Produto> entities = await EntitiesAsync(
+                query: query,
+                parameters: null);
+
+            return entities;
+        }
+    }
+}
